7128a273856d34e4392b9a41123c269a93287d43
Sampo Malinen <128909280+steffe25@users.noreply.github.com>
2024-07-09 15:52:27 +0300

    Add files via upload

---
+++ contactme.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Plugin Name: contactme
+Description: A simple contact form plugin.
+Version: 1.0
+Author: Sampo Malinen
+*/
+
+// Include necessary files
+include_once plugin_dir_path(__FILE__) . 'includes/functions.php';
+
+// Enqueue styles
+function my_contact_form_styles() {
+    wp_enqueue_style('my-contact-form-styles', plugins_url('assets/css/style.css', __FILE__));
+}
+add_action('wp_enqueue_scripts', 'my_contact_form_styles');
+
+// Add the shortcode
+function my_contact_form() {
+    $form = '
+    <form action="' . esc_url($_SERVER['REQUEST_URI']) . '" method="post">
+        <p>
+            Nimi <br/>
+            <input type="text" name="cf-name" pattern="[a-zA-Z0-9 ]+" value="' . (isset($_POST["cf-name"]) ? esc_attr($_POST["cf-name"]) : '') . '" size="40" />
+        </p>
+        <p>
+            Sähköposti <br/>
+            <input type="email" name="cf-email" value="' . (isset($_POST["cf-email"]) ? esc_attr($_POST["cf-email"]) : '') . '" size="40" />
+        </p>
+        <p>
+            Otsikko <br/>
+            <input type="text" name="cf-subject" pattern="[a-zA-Z ]+" value="' . (isset($_POST["cf-subject"]) ? esc_attr($_POST["cf-subject"]) : '') . '" size="40" />
+        </p>
+        <p>
+            Kuinka voin auttaa? <br/>
+            <textarea rows="10" cols="35" name="cf-message">' . (isset($_POST["cf-message"]) ? esc_attr($_POST["cf-message"]) : '') . '</textarea>
+        </p>
+        <p><input type="submit" name="cf-submitted" value="Lähetä"></p>
+    </form>
+    ';
+    return $form;
+}
+add_shortcode('my_contact_form', 'my_contact_form');
---
+++ includes/functions.php
@@ -0,0 +1,32 @@
+<?php
+function deliver_mail() {
+    // If the submit button is clicked, send the email
+    if (isset($_POST['cf-submitted'])) {
+        
+        // Sanitize form values
+        $name    = sanitize_text_field($_POST["cf-name"]);
+        $email   = sanitize_email($_POST["cf-email"]);
+        $subject = sanitize_text_field($_POST["cf-subject"]);
+        $message = esc_textarea($_POST["cf-message"]);
+        
+        // Get the blog administrator's email address
+        $to = get_option('admin_email');
+        
+        $headers = array('Content-Type: text/html; charset=UTF-8', "From: $name <$email>");
+        
+        // If email has been process for sending, display a success message
+        if (wp_mail($to, $subject, $message, $headers)) {
+            echo '<div>';
+            echo '<p>Thanks for contacting us, expect a response soon.</p>';
+            echo '</div>';
+        } else {
+            echo 'An unexpected error occurred';
+            
+            // Log error to debug.log
+            if (WP_DEBUG) {
+                error_log('Contact form submission error: Email failed to send.');
+            }
+        }
+    }
+}
+add_action('wp_head', 'deliver_mail');
